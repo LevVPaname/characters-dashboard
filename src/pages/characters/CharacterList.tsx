b2a9fd146f33b2e720e1c654245e0fee6cc77cd1
Vladislava <levVPaname@yandex.ru>
2026-05-30 13:18:22 +0300

    добавлено демо

--- src/pages/characters/CharacterList.tsx
+++ src/pages/characters/CharacterList.tsx
@@ -1,10 +1,13 @@
+import React, { useState } from 'react';
 import { Link } from 'react-router';
 
 import Image from '../../shared/assets/R_n_M_page_logo.png';
+import { Select } from '../../shared/ui/select/Select';
 
 import './CharacterList.css';
 
 export function CharacterList() {
+  const [selected, setSelected] = useState<string>();
   return (
     <div className='CharacterList'>
       <img
@@ -14,6 +17,39 @@ export function CharacterList() {
       />
       <p>Здесь будет список персонажей.</p>
       <Link to='/characters/1'>Открыть персонажа #1</Link>
+
+      <div style={{ display: 'flex', gap: '20px', marginTop: '20px' }}>
+        <Select
+          onChange={setSelected}
+          value={selected}
+          options={[
+            { label: 'Чел', value: '111' },
+            { label: 'Моб', value: '222' },
+            { label: 'Босс', value: '333' }
+          ]}
+          getOptionCustom={(option) => (
+            <span style={{ color: option.value === '111' ? 'red' : 'black' }}>
+              {option.label}
+            </span>
+          )}
+        />
+
+        <Select
+          onChange={setSelected}
+          size='small'
+          value={selected}
+          options={[
+            { label: 'Чел', value: '111' },
+            { label: 'Моб', value: '222' },
+            { label: 'Босс', value: '333' }
+          ]}
+          getOptionCustom={(option) => (
+            <span style={{ color: option.value === '111' ? 'red' : 'black' }}>
+              {option.label}
+            </span>
+          )}
+        />
+      </div>
     </div>
   );
 }
